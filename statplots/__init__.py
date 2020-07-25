76da94a45e1307f154660c0aae64bada0909f62d
Nick Paavola <nick.paavola@gmail.com>
2020-07-25 11:15:33 -0500

    use flask-talisman

--- statplots/__init__.py
+++ statplots/__init__.py
@@ -2,6 +2,17 @@ from flask import Flask
 from flask_talisman import Talisman
 
 app = Flask(__name__)
-Talisman(app)
+
+csp = {
+  'default-src': [
+    '\'self\'',
+    'code.jquery.com',
+    'cdn.jsdelivr.net',
+    'stackpath.bootstrapcdn.com',
+    'fonts.googleapis.com',
+    'cdnjs.cloudflare.com'
+  ]
+}
+talisman = Talisman(app, content_security_policy=csp)
 
 from statplots import routes
